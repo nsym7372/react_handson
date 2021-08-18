1065651b3c707bd9f3038302fb6c8d315539d0b0
nsym7372 <images7372@outlook.com>
2021-08-18 11:55:00 +0000

    6章をまとめた

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App0603_controlled';
+import App from './App0603';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
