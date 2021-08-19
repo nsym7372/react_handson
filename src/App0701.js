20854332e54e18d60d9e968f068146eac109b3d3
nsym7372 <images7372@outlook.com>
2021-08-19 06:12:57 +0000

    useEffectフック

---
+++ src/App0701.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Checkbox from './components/0701/Checkbox';
+export default function App() {
+
+    return (
+        <Checkbox />
+    );
+}
---
+++ src/components/0701/Checkbox.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+export default function Checkbox() {
+    const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        alert(`checked: ${checked.toString()}`);
+    });
+
+    // alert(`${checked.toString()}`);
+
+    return (
+        <>
+            <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
+            {checked ? "checked" : "not checked"}
+        </>
+        
+    );
+
+}
