8c519652fc0db0696dcac6f57bd071afd970a091
nsym7372 <images7372@outlook.com>
2021-08-23 02:33:57 +0000

    axiosでstore

--- src/components/08/Store.js
+++ src/components/08/Store.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 export default function Store() {
@@ -6,19 +7,26 @@ export default function Store() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
 
+    const createUser = async() => {
+        await axios.post('http://localhost:8080/api/public/api/users', {name, email, password});
+    }
+
     const submit = e => {
         e.preventDefault();
 
         // alert(title + ' : ' + color);
-        fetch('http://localhost:8080/api/public/api/users', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                name,
-                email,
-                password
-            })
-        })
+        // fetch('http://localhost:8080/api/public/api/users', {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({
+        //         name,
+        //         email,
+        //         password
+        //     })
+        // })
+
+        createUser();
+        
 
         setName("");
         setEmail("");
