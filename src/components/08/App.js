3b078c35544867251865e81204dbd43deb169d0a
nsym7372 <images7372@outlook.com>
2021-08-23 01:58:15 +0000

    reqct-query で get

--- src/components/08/App.js
+++ src/components/08/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Fetch from "./FetchTable";
 import Store from './Store';
 
+const queryClient = new QueryClient();
 export default function App() {
 
     return (
         <>
-            <Fetch />
-            <Store />
+            <QueryClientProvider client={queryClient}>
+                <Fetch />
+                <Store />
+            </QueryClientProvider>
         </>
     );
 }
--- src/components/08/FetchTable.js
+++ src/components/08/FetchTable.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
+import { useQuery } from "react-query";
 
 export default function FetchTable() {
     const [users, setUsers] = useState([]);
@@ -16,27 +17,44 @@ export default function FetchTable() {
     //         setUsers(data);
     //     }, [])
 
-    const f = async () => {
-        const { data } = await axios.get("http://localhost:8080/api/public/api/users");
-        setUsers(data);
-    }
+    // const f = async () => {
+    //     const { data } = await axios.get("http://localhost:8080/api/public/api/users");
+    //     setUsers(data);
+    // }
 
+    const {data, status} = useQuery('users', async () => {
+        const { data } = await axios.get("http://localhost:8080/api/public/api/users")
+        return data;
+    });
 
-    useEffect(() => {
-        // 純粋関数で無ければならない　https://qiita.com/daishi/items/4423878a1cd7a0ab69eb
-        f();
-        console.log('index');
-    }, []);
+    if(status === 'loading'){ return 'loading'}
+    else if(status === 'error'){return 'error'}
+
+    // useEffect(() => {
+    //     // 純粋関数で無ければならない　https://qiita.com/daishi/items/4423878a1cd7a0ab69eb
+    //     f();
+    //     console.log('index');
+    // }, []);
 
     return (
         <ul>
             {
-                users.map(user => {
+                data.map(user => {
                     return (
                         <li key={user.id}>{user.id} : {user.name}</li>
                     )
                 })
             }
         </ul>
+
+        // <ul>
+        //     {
+        //         users.map(user => {
+        //             return (
+        //                 <li key={user.id}>{user.id} : {user.name}</li>
+        //             )
+        //         })
+        //     }
+        // </ul>
     )
 }
