24f4a32de1d11e46092a3b16ef7f7063b24317a4
nsym7372 <images7372@outlook.com>
2021-08-23 01:36:15 +0000

    getをaxiosで記載、react-queryインストール

--- src/components/08/FetchTable.js
+++ src/components/08/FetchTable.js
@@ -1,17 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
 export default function FetchTable() {
     const [users, setUsers] = useState([]);
 
+    // const f = async () => {
+    //     await fetch(`http://localhost:8080/api/public/api/users`)
+    //         .then(res => res.json())
+    //         .then(data => { setUsers(data) })
+    // };
+
+    // const f = useCallback(
+    //     async() => {
+    //         const {data} = await axios.get("http://localhost:8080/api/public/api/users");
+    //         setUsers(data);
+    //     }, [])
+
+    const f = async () => {
+        const { data } = await axios.get("http://localhost:8080/api/public/api/users");
+        setUsers(data);
+    }
+
+
     useEffect(() => {
         // 純粋関数で無ければならない　https://qiita.com/daishi/items/4423878a1cd7a0ab69eb
-        const f = async () => {
-            await fetch(`http://localhost:8080/api/public/api/users`)
-                .then(res => res.json())
-                .then(data => { setUsers(data) })
-        }
         f();
-
+        console.log('index');
     }, []);
 
     return (
